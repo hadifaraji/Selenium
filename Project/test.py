a34732855e13dbf57662337f1d7841e8dd2f2812
user <hadifaraji026@gmail.com>
2024-05-28 20:43:33 +0430

    tset

--- Project/test.py
+++ Project/test.py
@@ -1,23 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.webdriver.common.alert import Alert
 import random
 
 driver = webdriver.Chrome()
+alert = Alert(driver)
+driver.get('https://www.flytoday.ir/')
 driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://dev02.flytoday.ir/")
-driver.find_element('xpath', "//*[text()='ورود']").click()
-driver.find_element('xpath', "//*[@name = 'emailOrMobile']").send_keys('09356671285')
-driver.find_element('xpath', "//*[text()='ادامه']").click()
-driver.find_element('xpath', "//*[@name='pass']").send_keys('Aa123456')
-sleep(0.5)
-driver.find_element('xpath', "//*[text()='ورود' and @type='primary']").click()
-sleep(0.5)
-driver.find_element('xpath', "//*[text()='ویزا']").click()
+
+driver.find_element(by=By.CSS_SELECTOR, value=".nav_navButtonRight__WIalm  ").click()
 sleep(5)
-driver.find_element('xpath' , "//*[text()='ویزا توریستی']").click()
-sleep(5)
-driver.execute_script('window.scrollTo(0,100)')
-sleep(4)
-x = driver.find_elements('xpath', "//*[@class='itinerary-list_itineraryList__5CLVr']")[0].click()
+driver.find_element(by=By.XPATH, value='//*[text()="مبدا"]').click()
+sleep(6)
+x = driver.find_elements(by=By.CLASS_NAME, value='autocomplete_optionsWrapper__FJAB3')
+print(x)
+y = random.choice(x)
+y.click()
 sleep(5)
--- Project/testt.py
+++ Project/testt.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 
+
 driver = webdriver.Chrome()
-url = 'https://www.yahoo.com/'
-driver.get(url)
-driver.execute_script('window.scrollTo(0,100)')
-sleep(4)
+driver.get('https://www.flytoday.ir/')
+driver.implicitly_wait(5)
+
+driver.find_element(by=By.XPATH,  value='//*[@id="deny"]').click()
+sleep(5)
