29586107cc561c3cb0912525cea2600fab5eed26
user <hadifaraji026@gmail.com>
2024-05-28 20:47:23 +0430

    tset

--- Project/testt.py
+++ Project/testt.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-
+from selenium.webdriver.common.alert import Alert
+from selenium import webdriver
+from time import sleep
 
 driver = webdriver.Chrome()
-driver.get('https://www.flytoday.ir/')
-driver.implicitly_wait(5)
 
-driver.find_element(by=By.XPATH,  value='//*[@id="deny"]').click()
+driver.get('https://www.flytoday.ir/')
 sleep(5)
+x = driver.switch_to.alert.dismiss()
+sleep(2)
