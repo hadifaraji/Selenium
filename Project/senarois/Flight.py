4fe23e05199c9fac2546b4b335430beb89f30e8b
user <hadifaraji026@gmail.com>
2024-05-28 22:48:56 +0430

    tset

--- Project/senarois/Flight.py
+++ Project/senarois/Flight.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from selenium.webdriver.common.alert import Alert
-from selenium import webdriver
-from time import sleep
+
 
 driver = webdriver.Chrome()
-
 driver.get('https://www.flytoday.ir/')
 sleep(5)
-x = driver.switch_to.alert.dismiss()
+click_on_flight = driver.find_element(by=By.CSS_SELECTOR, value='.nav_navButtonRight__WIalm  ').click()
+click_on_source = driver.find_element(by=By.XPATH, value="//*[text()='مبدا']").click()
 sleep(2)
+get_list_city = driver.find_elements(by=By.XPATH, value="//*[@class='autocomplete_optionsWrapper__FJAB3' ]//*[@role='button']")
+print(get_list_city)
