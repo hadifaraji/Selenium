9a048ff8130f4ba699ace621923f63363cde80e6
Hadi Faraji <H.Faraji@Abnoosgasht.org>
2024-05-29 17:56:53 +0330

    test

--- Project/senarois/Flight.py
+++ Project/senarois/Flight.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 from Project.modules.click_on_city import City
+import random
 
 driver = webdriver.Chrome()
 driver.get('https://flytoday.ir/')
@@ -13,7 +14,17 @@ City(driver).click_on_city_in_flight_destination(By, sleep)
 departure_date = driver.find_element(by=By.XPATH, value='//*[text()="تاریخ رفت"]').click()
 sleep(2)
 date_list_of_day = driver.find_elements(by=By.XPATH, value='//*[@class="day_day__G5CoD"]')
-print(len(date_list_of_day))
+count_of_date = len(date_list_of_day)
+enable_day = []
+for i in range(count_of_date):
+    if date_list_of_day[i].is_enabled():
+        enable_day.append(date_list_of_day[i])
+    else:
+        None
+chiose_random = random.choice(enable_day)
+chiose_random.click()
+sleep(5)
+click_on_approve = driver.find_element()
 
 
 
